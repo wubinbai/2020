e1e450566e8adffb0f2d4874e26d8bea30bfee3c
Wubin <wubinbai@yahoo.com>
2020-02-08 09:12:18 +0800

    cudnn pack

--- ubuntu_related/cudnn/cudnn_links.sh
+++
@@ -1,8 +0,0 @@
-sudo cp /usr/local/cuda-9.0/lib64/libcudart.so.9.0 /usr/local/lib/libcudart.so.9.0 && sudo ldconfig
-sudo cp /usr/local/cuda-9.0/lib64/libcublas.so.9.0 /usr/local/lib/libcublas.so.9.0 && sudo ldconfig
-sudo cp /usr/local/cuda-9.0/lib64/libcurand.so.9.0 /usr/local/lib/libcurand.so.9.0 && sudo ldconfig
-sudo cp /usr/local/cuda-9.0/lib64/libcudnn.so.7 /usr/local/lib/libcudnn.so.7 && sudo ldconfig
-
-sudo cp /usr/local/cuda-9.0/lib64/libcusolver.so.9.0 /usr/local/lib/libcusolver.so.9.0 && sudo ldconfig
-
-sudo cp /usr/local/cuda-9.0/lib64/libcufft.so.9.0 /usr/local/lib/libcufft.so.9.0 && sudo ldconfig
