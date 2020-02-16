5f6c684f280c77bdfa199d39def030ed9ec89204
wb <wubinbai@yahoo.com>
2020-02-16 10:06:42 +0800

    t info

--- kaggle/fsd2019/my_own_codes/preprocess.py
+++ kaggle/fsd2019/my_own_codes/preprocess.py
@@ -24,22 +24,26 @@ test_paths = TEST + test['fname']
 
 def get_arr_classes(df):
     res = []
+    count = 0
     for row in df.labels.values:
+        split = row.split(',')
+        num_labels = len(split)
         res.append(row.split(',')[0])
+        count+=1
     res_df = pd.DataFrame(res)
-    s = res_df = res_df[0] # Series.unique()
+    y0 = res_df = res_df[0] # Series.unique()
     res_df = res_df.unique()
-    return res_df, s
+    return res_df, y0
 
 
-arr_classes_curated, s_curated = get_arr_classes(curated)
-arr_classes_noisy, s_noisy = get_arr_classes(noisy)
+arr_classes_curated, y0_curated = get_arr_classes(curated)
+arr_classes_noisy, y0_noisy = get_arr_classes(noisy)
 #arr_classes_test = get_arr_classes(test)
 
 # modified
 curated_m = curated.copy()
 noisy_m = noisy.copy()
 
-curated_m['y0'] = s_curated
-noisy_m['y0'] = s_noisy
+curated_m['y0'] = y0_curated
+noisy_m['y0'] = y0_noisy
 
--- kaggle/fsd2019/my_own_codes/t_info.py
+++ kaggle/fsd2019/my_own_codes/t_info.py
@@ -5,6 +5,7 @@ import librosa
 import os
 PREFIX = '../'
 dir_80 = PREFIX+'80_classes'
+import numpy as np
 
 def get_t_info():
     os.chdir(dir_80)
@@ -16,12 +17,21 @@ def get_t_info():
             os.chdir(sub)
             # do whatever you want
             fs = os.listdir()
-            ts_one_class = []
-            for f in tqdm(fs):
-                y, sr = librosa.load(i)
+            ts_i = []
+            print('for running all samples, modify to fs[:]')
+            for f in tqdm(fs[:10]):
+                y, sr = librosa.load(f)
                 t = librosa.get_duration(y,sr)
-                ts_one_class.append(t)
-            assert len(ts_one_class) == len(fs)
+                ts_i.append(t)
+            t_mean = np.mean(ts_i)
+            t_std = np.std(ts_i)
+            t_min = np.min(ts_i)
+            t_max = np.max(ts_i)
+            num_files = len(fs)
+            # after analysis, make a statistics dir that stores info
+            os.mkdir('statistics')
+            stats = np.array([ts_i,t_mean,t_std,t_min,t_max,num_files])
+            np.save('./statistics/stats.npy',stats)
 
         # After exploring one of curated/noisy path, go out.
         os.chdir('../')
