41bbbb2ed380dbf8c0c8deeea99f0ad9c6438211
wb <wubinbai@yahoo.com>
2020-02-23 18:13:58 +0800

    mir

---
+++ Music/MIR/mir.py
@@ -0,0 +1,37 @@
+import librosa
+hop_length = 256
+frame_length = 512
+import librosa.display as disp
+
+x, sr = librosa.load('audio/simple_loop.wav')
+y = x
+energy = np.array([sum((x[i:i+frame_length]**2)) for i in range(0,len(y),hop_length)])
+frames = range(len(energy))
+t = librosa.frames_to_time(frames,sr,hop_length=hop_length)
+
+def strip(x, frame_length, hop_length):
+
+    # Compute RMSE.
+    rms = librosa.feature.rms(x, frame_length=frame_length, hop_length=hop_length, center=True)
+    
+    # Identify the first frame index where RMSE exceeds a threshold.
+    thresh = 0.01
+    frame_index = 0
+    while rms[0][frame_index] < thresh:
+        frame_index += 1
+        
+    # Convert units of frames to samples.
+    start_sample_index = librosa.frames_to_samples(frame_index, hop_length=hop_length)
+    
+    # Return the trimmed signal.
+    return x[start_sample_index:]
+
+#y = strip(x, frame_length, hop_length)
+X = librosa.stft(y)
+Xdb = librosa.amplitude_to_db(abs(X))
+#plt.ion()
+#disp.specshow(Xdb,sr=sr,x_axis='time',y_axis='hz')
+#plt.figure()
+#plt.show()
+
+rms = librosa.feature.rms(y)
