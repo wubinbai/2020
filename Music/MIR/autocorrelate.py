6420c5fc932e23d511e6238c7fb4be86503836d9
Wubin <wubinbai@yahoo.com>
2020-03-01 17:14:43 +0800

    autoco 2

--- Music/MIR/autocorrelate.py
+++ Music/MIR/autocorrelate.py
@@ -10,9 +10,33 @@ def ac(file):
     r[int(i_max):]=0
     print('r.shape',r.shape)
     argmax = r.argmax()
+    # algo my 
+    localmax = int(i_min)
+    temp = localmax+1
+    start = True
+    while start:
+        if r[temp] < r[localmax]:
+            decrease = True
+        else:
+            decrease = False
+        if decrease == False:
+            increase = True
+            break
+        localmax+=1
+        temp+=1
+    while increase:
+        localmax+=1
+        temp+=1
+        if r[temp] > r[localmax]:
+            increase = True
+        else:
+            increase = False
+        if increase == False:
+            break
+
     print('r.argmax()',argmax)
     f0 = float(sr)/argmax
-    return (f0,r,R_B,argmax,y)
+    return ((f0,localmax),r,R_B,argmax,y)
 
 def plot_ac(ac0,coef):
     plt.figure()
@@ -28,3 +52,7 @@ def plot_ac(ac0,coef):
 f = 'C5s.wav'
 ac0=ac(f)
 plot_ac(ac0,1500)
+
+f2 = 'A4C5s.wav'
+ac2 = ac(f2)
+plot_ac(ac2,1500)
