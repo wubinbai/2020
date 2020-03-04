4f300d2124694d4b4517b2acf8e7efc607ccd2b9
Wubin <wubinbai@yahoo.com>
2020-03-04 21:09:30 +0800

    iterstrat 2

--- kaggle/fsd2019/2nd_place_playground/iterstrat/eg.py
+++ kaggle/fsd2019/2nd_place_playground/iterstrat/eg.py
@@ -1,4 +1,7 @@
 from iterstrat.ml_stratifiers import RepeatedMultilabelStratifiedKFold
+from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
+
+
 import numpy as np
 
 X = np.array([[1,2], [3,4], [1,2], [3,4], [1,2], [3,4], [1,2], [3,4]])
@@ -10,7 +13,7 @@ for train_index, test_index in rmskf.split(X, y):
    print("TRAIN:", train_index, "TEST:", test_index)
    X_train, X_test = X[train_index], X[test_index]
    y_train, y_test = y[train_index], y[test_index]
-
+print('---')
 msss = MultilabelStratifiedShuffleSplit(n_splits=3, test_size=0.5, random_state=0)
 
 for train_index, test_index in msss.split(X, y):
