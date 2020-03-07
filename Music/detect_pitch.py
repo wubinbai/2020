10e7b135a073b5d69ca2afeccdabe83b45af86dc
wb <wubinbai@yahoo.com>
2020-03-07 15:01:44 +0800

    pitches

--- Music/detect_pitch.py
+++ Music/detect_pitch.py
@@ -7,3 +7,14 @@ def detect_pitch(y, sr, t):
   pitch = pitches[index, t]
 
   return pitch
+
+
+def get_pitches():
+    res = []
+    tl = pitches.shape[1]
+    for t in range(tl):
+        pitch = detect_pitch(y,sr,t)
+        res.append(pitch)
+    return res
+
+
