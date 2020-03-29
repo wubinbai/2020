80c354c11fd92ef97dbff62bb4ea5bbd8d4030e6
wb <wubinbai@yahoo.com>
2020-03-29 11:28:21 +0800

    harmonica

--- Music/my_fsd_file/Instrument/Hi-hat/ana.py
+++ Music/my_fsd_file/Instrument/Hi-hat/ana.py
@@ -4,20 +4,24 @@ import librosa.display as disp
 #f = '95f92a43.wav'
 f = librosa.util.find_files('.')[0]
 y,sr = librosa.load(f)
-plt.figure()
-disp.waveplot(y)
-plt.savefig('wave.png')
 y_h, y_p = librosa.effects.hpss(y)
 plt.figure()
 plt.subplot(2,1,1)
 disp.waveplot(y_h)
 plt.subplot(2,1,2)
 disp.waveplot(y_p)
+plt.tight_layout()
 plt.savefig('hpss.png')
 rms = librosa.feature.rms(y)[0]
 plt.figure()
 plt.plot(rms)
 plt.savefig('rms.png')
+plt.figure()
+disp.waveplot(y)
+max_rms = np.argmax(rms)
+locate = 512 * max_rms
+plt.vlines(locate,0,max(y))
+plt.savefig('wave.png')
 zcr = librosa.feature.zero_crossing_rate(y)[0]
 plt.figure()
 plt.plot(zcr)
